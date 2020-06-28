86096472ef2794e5f3f1f61cb78731faff09287f
Méloé <meloscavezzoni@gmail.com>
2020-06-28 20:05:33 +0200

    Routing in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 
 // SERVICES
 import { AuthService } from './services/auth.service';
@@ -16,6 +20,15 @@ import { SingleBookComponent } from './book-list/single-book/single-book.compone
 import { BookFormComponent } from './book-list/book-form/book-form.component';
 import { HeaderComponent } from './header/header.component';
 
+// Routing
+const appRoutes: Routes = [
+  { path: 'auth/sinup', component: SignupComponent },
+  { path: 'auth/sinin', component: SigninComponent },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'books/view/:id', component: SingleBookComponent },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +39,14 @@ import { HeaderComponent } from './header/header.component';
     BookFormComponent,
     HeaderComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [AuthService, AuthGuardService, BooksService],
   bootstrap: [AppComponent],
 })
