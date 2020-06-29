ac8e7d747c9b0b9df086052f61c30172ef501d28
Méloé <meloscavezzoni@gmail.com>
2020-06-29 21:30:42 +0200

    Auth guard

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(
+        // Return a user
+        (user) => {
+          if (user) {
+            // If user : user can pass
+            resolve(true);
+          } else {
+            // We redirect
+            this.router.navigate(['/auth', 'signin']);
+            // user can't pass
+            resolve(false);
+          }
+        }
+      );
+    });
+  }
 }
