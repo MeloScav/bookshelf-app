321fd8e21c0b28bdf630149f309458cbceb4f3b7
Méloé <meloscavezzoni@gmail.com>
2020-06-29 11:51:18 +0200

    Firebase: set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bookshelf-app';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyBtXRPwRZ4ctO40YXQ3l-a7Ttvai9QGKwY',
+      authDomain: 'bookshelf-2f209.firebaseapp.com',
+      databaseURL: 'https://bookshelf-2f209.firebaseio.com',
+      projectId: 'bookshelf-2f209',
+      storageBucket: 'bookshelf-2f209.appspot.com',
+      messagingSenderId: '189710992428',
+      appId: '1:189710992428:web:1f2d4528da072f4f647d89',
+      measurementId: 'G-8PNC32MEYN',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
