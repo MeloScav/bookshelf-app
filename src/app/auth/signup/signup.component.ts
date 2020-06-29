650b66b61fadef1b8031cd83e956fbf8ef310ff6
Méloé <meloscavezzoni@gmail.com>
2020-06-29 14:38:01 +0200

    Form initialization in ts

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  // FORM
+  signUpForm: FormGroup;
+  // If error
+  errorMessage: string;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    // Form initialization
+    this.initForm();
   }
 
+  initForm() {
+    // Password: min 6 characters
+    this.signUpForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)],
+      ],
+    });
+  }
 }
