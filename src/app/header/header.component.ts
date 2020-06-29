e8c6ef506e7304fcc52ac7ace497892e40833930
Méloé <meloscavezzoni@gmail.com>
2020-06-29 19:28:29 +0200

    User detected

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  // Local variable : authentification status
+  isAuth: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    // Activated when authentification status changes
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // If user is detected
+        this.isAuth = true;
+      } else {
+        this.isAuth = false;
+      }
+    });
   }
-
 }
