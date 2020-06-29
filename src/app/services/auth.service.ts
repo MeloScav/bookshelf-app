20a0f8281f81092ac277e742a93bfe148473efe2
Méloé <meloscavezzoni@gmail.com>
2020-06-29 11:59:06 +0200

    Create a new user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor() {}
 
-  constructor() { }
+  // Create a new user
+  // Asynchronous method
+  createNewUser(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  }
 }
