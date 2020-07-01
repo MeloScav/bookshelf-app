780307417f9e03891e60ebef25d85155568e490e
Méloé <meloscavezzoni@gmail.com>
2020-07-01 22:49:11 +0200

    Initialization

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../models/book.model';
+import { Subscription } from 'rxjs';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.scss'],
 })
 export class BookListComponent implements OnInit {
+  // Local array
+  books: Book[];
+  // To subscribe to subject of books.service
+  booksSubscription: Subscription;
 
-  constructor() { }
+  constructor(private booksService: BooksService, private router: Router) {}
 
   ngOnInit(): void {
+    // Updates the local array
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.booksService.emitBooks();
   }
-
 }
