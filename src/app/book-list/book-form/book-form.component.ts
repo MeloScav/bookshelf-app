00b7e0074426253752e55258c96f6bcb1527e8b6
Méloé <meloscavezzoni@gmail.com>
2020-07-02 13:39:10 +0200

    Initialize the form

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { BooksService } from 'src/app/services/books.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
-  styleUrls: ['./book-form.component.scss']
+  styleUrls: ['./book-form.component.scss'],
 })
 export class BookFormComponent implements OnInit {
+  bookForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private booksService: BooksService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    // Initialize the form
+    this.initForm();
   }
 
+  initForm() {
+    this.bookForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+    });
+  }
 }
