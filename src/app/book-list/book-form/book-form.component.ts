d880c424cb2954aeb80e3d387f037440ad2c7a4d
Méloé <meloscavezzoni@gmail.com>
2020-07-02 14:37:40 +0200

    Save new book

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { BooksService } from 'src/app/services/books.service';
 import { Router } from '@angular/router';
+import { Book } from 'src/app/models/book.model';
 
 @Component({
   selector: 'app-book-form',
@@ -27,5 +28,19 @@ export class BookFormComponent implements OnInit {
       title: ['', Validators.required],
       author: ['', Validators.required],
     });
+  }
+
+  // SAVE BOOK
+  onSaveBook() {
+    // Recover the form values
+    const title = this.bookForm.get('title').value;
+    const author = this.bookForm.get('author').value;
+
+    // New book and save
+    const newBook = new Book(title, author);
+    this.booksService.createNewBook(newBook);
+
+    // Return to list of books
+    this.router.navigate(['/books']);
   }
 }
