4743347ddde2978f3082da4b4a9c1c2501577174
Méloé <meloscavezzoni@gmail.com>
2020-07-05 18:29:56 +0200

    Delete style path

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -6,7 +6,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent implements OnInit {
   // FORM
