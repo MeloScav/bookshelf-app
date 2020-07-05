061c3b0583dd44e3470020a13474f5191c503ca6
Méloé <meloscavezzoni@gmail.com>
2020-07-05 16:27:02 +0200

    Styles url

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,6 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   // Local variable : authentification status
